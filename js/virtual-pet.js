731818525bffd496c4275c52b4960f8743a790f1
Kristen Collier <kristencollier10@gmail.com>
2020-10-12 21:39:44 -0500

    virtual pet updates

--- js/virtual-pet.js
+++ js/virtual-pet.js
@@ -1,8 +1,12 @@
 $(function() {
 
-    "use strict";
-
     // Add a variable "pet_info" equal to a object with the name (string), weight (number), and happiness (number) of your pet
+
+    let pet_info = {
+        name: "Chief",
+        weight: 80,
+        happiness: 100
+    }
 
     // Called function to update the name, happiness, and weight of our pet in our HTML
     checkAndUpdatePetInfoInHtml();
@@ -10,18 +14,22 @@ $(function() {
     // When each button is clicked, it will "call" function for that button (functions are below)
     $('.treat-button').click(clickedTreatButton);
     $('.play-button').click(clickedPlayButton);
-    // $('.exercise-button').click(clickedExerciseButton);
+    $('.exercise-button').click(clickedExerciseButton);
 
 
 
 
     function clickedTreatButton() {
-        // Increase pet happiness
-        // Increase pet weight
+        pet_info.happiness = pet_info.happiness + 1;
+        pet_info.weight = pet_info.weight + 1;
+
+
         checkAndUpdatePetInfoInHtml();
     }
 
     function clickedPlayButton() {
+        pet_info.weight = pet_info.weight - 1;
+        pet_info.happiness = pet_info.happiness + 3
         // Increase pet happiness
         // Decrease pet weight
         checkAndUpdatePetInfoInHtml();
@@ -29,6 +37,8 @@ $(function() {
 
     function clickedExerciseButton() {
         // Decrease pet happiness
+        pet_info.happiness = pet_info.happiness - 2;
+        pet_info.weight = pet_info.weight - 3;
         // Decrease pet weight
         checkAndUpdatePetInfoInHtml();
     }
@@ -40,6 +50,9 @@ $(function() {
 
     function checkWeightAndHappinessBeforeUpdating() {
         // Add conditional so if weight is lower than zero, set it back to zero
+        if (pet_info.weight >= 0) {
+            return pet_info.weight === 0;
+        }
     }
 
     // Updates your HTML with the current values in your pet_info object
